8a4f299de0582178726dcd882ccb270a5f273e41
Jonathan Speiser <jspeis@gmail.com>
2017-02-06 10:11:34 -0500

    rename json file to js

--- gen_words.py
+++ gen_words.py
@@ -3,5 +3,5 @@ import os
 import json
 tmp = [{"word": x.split(".mp4")[0]} for x in os.listdir('words') if ".mp4" in x]
 output_str = "var words = {};".format(json.dumps(tmp))
-with open("words.json", "w") as f:
+with open("words.js", "w") as f:
     f.write(output_str)
